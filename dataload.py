932dbc599b33d8f39071a5b76d5f32c775502a5a
Jhonnathan93 <jsocampod@eafit.edu.co>
2024-08-14 20:47:09 -0500

    Format times

--- dataload.py
+++ dataload.py
@@ -1,11 +1,12 @@
 import os
-from multiprocessing import Process, current_process
+from multiprocessing import Process
 import time
 import argparse
 import psutil
 
 # Constante que define el tamaño de página en bytes (4 KB)
 PAGE_SIZE = 4096
+
 
 def get_cpu_core(pid):
     """
@@ -14,7 +15,8 @@ def get_cpu_core(pid):
     try:
         with open(f'/proc/{pid}/stat') as f:
             fields = f.read().split()
-            cpu_core = fields[38]  # El índice 38 corresponde al núcleo de la CPU
+            cpu_core = fields[
+                38]  # El índice 38 corresponde al núcleo de la CPU
             return int(cpu_core)
     except FileNotFoundError:
         print(f"El proceso con PID {pid} no existe.")
@@ -22,6 +24,7 @@ def get_cpu_core(pid):
     except Exception as e:
         print(f"Error al leer el núcleo de la CPU para el proceso {pid}: {e}")
         return None
+
 
 def buscar_archivos_csv(directorio, extension='.csv'):
     """
@@ -33,12 +36,14 @@ def buscar_archivos_csv(directorio, extension='.csv'):
             archivos_csv.append(os.path.join(directorio, archivo))
     return archivos_csv
 
+
 def set_cpu_affinity(cpu_id):
     """
     Establece la afinidad de la CPU para el proceso actual a una CPU específica.
     """
     pid = os.getpid()
     os.sched_setaffinity(pid, {cpu_id})
+
 
 def get_memory_usage(pid):
     """
@@ -47,6 +52,7 @@ def get_memory_usage(pid):
     process = psutil.Process(pid)
     memory_info = process.memory_info()
     return memory_info.rss // 1024  # Retorna la memoria en KB
+
 
 def read_csv(file_path):
     """
@@ -80,66 +86,95 @@ def read_csv(file_path):
     elapsed_time = (end_time - start_time) * 1000  # Convertir a milisegundos
 
     # Formatear la salida en forma de tabla
-    print(f"{pid:<10} {cpu_core:<5} {os.path.basename(file_path):<20} {page_number:<10} {elapsed_time:<20.6f} {memory_usage_start:<15} {memory_usage_end:<15}")
+    print(
+        f"{pid:<10} {cpu_core:<5} {os.path.basename(file_path):<20} {page_number:<10} {elapsed_time:<20.6f} {memory_usage_start:<15} {memory_usage_end:<15}"
+    )
 
     return pages
+
 
 def main():
     """
     Función principal que controla el flujo del programa.
     """
-    parser = argparse.ArgumentParser(description="Simulación de lectura de archivos CSV con paginación.")
-    parser.add_argument('-s', '--single', action='store_true', help="Single mode.")
-    parser.add_argument('-m', '--multi', action='store_true', help='Multi mode.')
-    parser.add_argument('-f', '--folder', type=str, help='Folder to search for CSV files.')
+    parser = argparse.ArgumentParser(
+        description="Simulación de lectura de archivos CSV con paginación.")
+    parser.add_argument('-s',
+                        '--single',
+                        action='store_true',
+                        help="Single mode.")
+    parser.add_argument('-m',
+                        '--multi',
+                        action='store_true',
+                        help='Multi mode.')
+    parser.add_argument('-f',
+                        '--folder',
+                        type=str,
+                        help='Folder to search for CSV files.')
 
     args = parser.parse_args()
     folder = args.folder if args.folder else os.getcwd()
 
     if args.single and args.multi:
-        print("No se pueden establecer ambas banderas al mismo tiempo (python dataload.py -s -m -f FOLDER)")
+        print(
+            "Cannot set both flags at the same time (python dataload.py -s -m -f FOLDER)"
+        )
         return 1
 
     if not os.path.isdir(folder):
-        print(f"El directorio '{folder}' no existe.")
+        print(f"The directory '{folder}' does not exist.")
         return 1
 
     # Obtener una lista de todos los archivos CSV en el directorio
     csv_files = buscar_archivos_csv(folder)
 
     if not csv_files:
-        print(f"No se encontraron archivos CSV en el directorio '{folder}'.")
+        print(f"No CSV files were found in the '{folder}' directory.")
         return 1
 
     pid = os.getpid()
     cpu_core = get_cpu_core(pid)
     if cpu_core is not None:
-        print(f"El proceso {pid} se está ejecutando en el núcleo {cpu_core}.")
+        print(f"Process {pid} is running on core {cpu_core}.")
 
     processes = []
 
     if args.single:
         set_cpu_affinity(cpu_core)
 
-    # Imprimir encabezados de la tabla
-    print(f"{'PID':<10} {'Core':<5} {'File':<20} {'Pages':<10} {'Time (ms)':<20} {'Mem Start (KB)':<15} {'Mem End (KB)':<15}")
-
     start_time_program = time.time()
+    print(
+        f"The program starts at {time.strftime('%H:%M:%S', time.localtime(start_time_program))}"
+    )
+
+    print(
+        f"{'PID':<10} {'Core':<5} {'File':<20} {'Pages':<10} {'Time (ms)':<20} {'Mem Start (KB)':<15} {'Mem End (KB)':<15}"
+    )
+
+    start_time_first_file = time.time()
 
     for file_path in csv_files:
-        process = Process(target=read_csv, args=(file_path,))
+        process = Process(target=read_csv, args=(file_path, ))
         processes.append(process)
         process.start()
 
     for process in processes:
         process.join()
 
-    end_time_program = time.time()
-    total_time = end_time_program - start_time_program
+    end_time_last_file = time.time()
+    print(f"Start time of the first file load: {time.strftime('%H:%M:%S', time.localtime(start_time_first_file))}")
+    print(
+        f"The program ended at {time.strftime('%H:%M:%S', time.localtime(end_time_last_file))}"
+    )
+
+    total_time = end_time_last_file - start_time_program
     minutes, seconds = divmod(total_time, 60)
-    print(f"Programa terminado en {minutes:02}:{seconds:02} segundos")
+    print(
+        f"The time used to read {len(csv_files)} files is: {minutes:02}:{seconds:06.3f} seconds"
+    )
 
     return 0
+
 
 if __name__ == '__main__':
     print('Return', main())
